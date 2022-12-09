ad4046cd9539ac43e753d64b4a82117bac3e4e3d
Roman <cheforoman@gmail.com>
2022-12-09 08:54:44 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Successs)
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
