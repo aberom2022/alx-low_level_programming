8a14a913b6b8c394976e2b122a731c2fcbffc7ee
Roman <cheforoman@gmail.com>
2022-12-13 12:37:37 -0800

    Hot fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,11 +10,9 @@ void times_table(void)
 	int num, mult, prod;
 
 	for (num = 0; num <= 9; num++)
-	{
 		_putchar('0');
 
 		for (mult = 1; mult <= 9; mult++)
-		{
 			_putchar('0');
 			_putchar(' ');
 
@@ -24,7 +22,4 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
-		}
-		_putchar('\n');
-	}
 }
