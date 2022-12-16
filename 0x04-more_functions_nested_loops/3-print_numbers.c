def9b647a4e04745f109a3d17d166936f13969a2
Roman <cheforoman@gmail.com>
2022-12-15 20:58:44 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0123456789\n
+ *
+ * Return: void
+ */
+
+int print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	{
+		_putchar('\n');
+	}
+}
