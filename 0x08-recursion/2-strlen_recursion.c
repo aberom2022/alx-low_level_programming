85009d1ccb263eacc176ccb262e472fcacca2356
Roman <cheforoman@gmail.com>
2023-01-04 11:07:59 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - return the length of a string
+ * _strlen_recursion - returns the length of a string
  * @s: the string to be measured
  *
  * Return: the length of the string
@@ -16,4 +16,3 @@ int _strlen_recursion(char *s)
 		len++;
 		len += _strlen_recursion(s + 1);
 	}
-}
