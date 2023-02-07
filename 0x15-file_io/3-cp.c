50c0ce4ca8a8bdff312c978f39bf46aa864943ff
Roman <cheforoman@gmail.com>
2023-02-06 16:22:19 -0800

    3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,10 +6,10 @@ char *create_buffer(char *file);
 void close_file(int fd);
 
 /**
- * create_buffer - allocates 1024 bytes for a buffer
- * @file: the name of the file buffer is storing chars for
+ * create_buffer - allocates 1024 bytes for buffer
+ * @file: athe name of the file buffer is storing chars for
  *
- * Return: a ponter to the newly-allocated buffer
+ * Return: a pointer to the newly-allocated buffer
  */
 
 char *create_buffer(char *file)
@@ -33,7 +33,7 @@ char *create_buffer(char *file)
  * @fd: the file descriptor to be closed
  */
 
-void close_files(int fd)
+void close_file(int fd)
 {
 	int c;
 
@@ -47,19 +47,19 @@ void close_files(int fd)
 }
 
 /**
- * main : copies the contents of a file to another file
+ * main - copies the contents of a file to another file
  * @argc: the number of arguments supplied to the program
- * @argv: an array of pointer to the arguments
+ * @argv: An array of pointer to the arguments
  *
  * Return: 0 on success
  *
  * Description: if the argument count is incorrect - exit code 97
- *              if file_from does not exist or cannot be read - exit code 98
+ *              if file_from does not exit or cannot be read - exit code 98
  *              if file_to cannot be created or written to - exit code 99
  *              if file_to or file_from cannot be closed - exit code 100
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv)
 {
 	int from, to, r, w;
 	char *buffer;
@@ -79,16 +79,16 @@ int main(int argc, char *argv[])
 		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
-					"Error: Can't read from file %s\n", argv[1]);
+					"Error: Can't read from file %d\n", argv[1]);
 			free(buffer);
-			exit(99);
+			exit(98);
 		}
 
 		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
-					"Error: Can't write to %s\n", argv[2]);
+					"Error: Can't write to %d\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
@@ -104,3 +104,5 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
+
+
